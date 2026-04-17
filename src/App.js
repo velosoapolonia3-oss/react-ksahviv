321fe4e9d19cd82124ae277cb5924a01431fc46f
velosoapolonia3-oss <velosoapolonia3@gmail.com>
2026-04-17 01:14:07 -0300

    final2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
       <main>
         <section id="sobre">
           <h2>Sobre Mim</h2>
-          {/* Coloque o link da foto da Lilena aqui se tiver */}
+          {}
           <img src="/imagem.jpg.jpeg" alt="Foto de Apolonia" className="foto" />
           <p>Sou estudante de Sistemas para Internet (UAPI), com interesse em desenvolvimento web e criação de soluções digitais organizadas e eficientes.</p>
         </section>
